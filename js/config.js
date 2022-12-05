24f8048adf4b3e10f820709e195b7319a83d6ba0
MohammedAk9 <77897095+MohammedAk9@users.noreply.github.com>
2022-12-04 23:17:11 -0600

    helolo

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId:         ' us-west-2_jRuZ5vMXs',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '36bft0nj4p82mlcbt55hmi8ugu',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-west-2'          // e.g. us-east-2
+    },
+    api: {
+        invokeUrl:          ' https://21yqdvg112.execute-api.us-west-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
